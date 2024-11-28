d487b00914f33695c7f1ad589e57f6cfef7ccb6d
melihdemir0 <melihdemirr0@gmail.com>
2024-11-28 13:27:17 +0300

    last

--- Users/src/main/java/tr/edu/ogu/ceng/User/controller/SettingController.java
+++ Users/src/main/java/tr/edu/ogu/ceng/User/controller/SettingController.java
@@ -1,27 +1,32 @@
 package tr.edu.ogu.ceng.User.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import tr.edu.ogu.ceng.User.dto.SettingDTO;
+import tr.edu.ogu.ceng.User.entity.Setting;
 import tr.edu.ogu.ceng.User.service.SettingService;
 
 @RestController
-@RequestMapping("/API/V1/Setting")
+@RequestMapping("/api/v1/setting")
 @RequiredArgsConstructor
 public class SettingController {
 
 	private final SettingService settingservice;
 
 	@GetMapping("/{id}")
-	public Object getSetting(@PathVariable Long id) {
-	    Object setting = settingservice.getSetting(id);
+	public ResponseEntity<SettingDTO> getSetting(@PathVariable Long id) {
+	    Setting setting = settingservice.getSetting(id);
 	    if (setting == null) {
 	        throw new RuntimeException("Setting not found for id: " + id);
 	    }
-	    return setting;
+	    SettingDTO settingDTO =new SettingDTO();
+	    settingDTO.setId(5);
+	    return ResponseEntity.ok(settingDTO);
 	}
 
 
--- Users/src/main/java/tr/edu/ogu/ceng/User/dto/SettingDTO.java
+++ Users/src/main/java/tr/edu/ogu/ceng/User/dto/SettingDTO.java
@@ -1,5 +1,9 @@
 package tr.edu.ogu.ceng.User.dto;
 
+import lombok.Data;
+
+@Data
 public class SettingDTO {
+long id;
 
 }
